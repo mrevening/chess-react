ea6fa921214984a0b33cbc0cb5b5b54d1ed3c03d
dwieczorek <dwieczorek@kpmg.pl>
2021-09-19 20:50:39 +0200

    refactor toward functions

--- src/board/BoardSlice.tsx
+++ src/board/BoardSlice.tsx
@@ -1,6 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction  } from '@reduxjs/toolkit'
 import type { RootState } from '../store'
-import {InitFigures, Figure, Player} from './Interface'
+import {InitFigures, Figure, Player, FigureType, RowLine, ColumnLine} from './Interface'
 
 interface BoardSlice {
   figures: Array<Figure>,
@@ -14,49 +14,119 @@ const initialState: BoardSlice = {
   currentPlayerTurn: Player.White
 }
 
-function changeTurn (turn: Player) { return turn === Player.White ? Player.Black : Player.White }
+interface ClickTile {
+  col: ColumnLine
+  row: RowLine
+}
+
+
 
 export const boardSlice = createSlice({
   name: 'board',
   initialState,
   reducers: {
-    clickTile: (state, action) => {
-      const {col, row} = action.payload
-      const clickedTileFigure = state.figures.find(f => f.Column === col && f.Row === row)
+    clickTile: (state, action: PayloadAction<ClickTile>) => {
+      const {col: clickedColumn, row: clickedRow} = action.payload
+      const clickedFigure = state.figures.find(f => f.Column === clickedColumn && f.Row === clickedRow)
+      function changeTurn (turn: Player) { return turn === Player.White ? Player.Black : Player.White }
+      function anyActiveFigure() { return state.activeFigure }
+      function isPlayersFigure() { return clickedFigure && clickedFigure.Player === state.currentPlayerTurn}
+      function isClickedFigureAndActiveFigureTheSame() { return state.activeFigure?.Id === clickedFigure?.Id}
+      function isEnemyFigureClicked() { return clickedFigure && clickedFigure.Player !== state.currentPlayerTurn}
+      function getOpponentsFigure() { return clickedFigure && state.figures.find(f => f.Id === clickedFigure.Id) }
 
-      if (!state.activeFigure){
-        if (clickedTileFigure?.Player === state.currentPlayerTurn){
-          state.activeFigure = clickedTileFigure
-          return state;
+      function playerClickedHisFigureFirstTime() { state.activeFigure = clickedFigure; }
+      function playerClickedThisFigureSecondTime() { state.activeFigure = undefined; }
+      function removeEnemyFigure() { state.figures = state.figures.filter(f => f.Id !== getOpponentsFigure()?.Id); }
+      function moveFigure() { 
+        const fi = state.figures.findIndex(f => f.Id === state.activeFigure?.Id)
+        state.figures[fi].Column = clickedColumn
+        state.figures[fi].Row = clickedRow
+        state.activeFigure = undefined
+        state.currentPlayerTurn = changeTurn(state.currentPlayerTurn);
+       }
+      function playerRemovesEnemysFigure () { 
+        removeEnemyFigure();
+        moveFigure();
+       }
+
+       function pawnMoves(activeFigure : Figure): boolean{
+        const direction = activeFigure.Player === Player.White ? -1 : 1
+        const lastLine = activeFigure.Player === Player.White ? RowLine.One : RowLine.Eight
+        const startingLine = activeFigure.Player === Player.White ? RowLine.Two : RowLine.Seven
+        const maxMove = (activeFigure.Row === startingLine) ? 2 : 1;
+
+        if (activeFigure.Column === clickedColumn){
+          if (state.figures.some(f => f.Row === clickedRow && f.Column === clickedColumn)) return false;
+          else if (activeFigure?.Row === clickedRow + 1*direction ) return true
+        }
+        else if (Math.abs(clickedColumn - activeFigure.Column) === 1){
+            const clickedEnemyFigure = state.figures.some(f => f.Row === clickedRow + direction && f.Column === clickedColumn && f.Player !== activeFigure.Player)
+             if (clickedEnemyFigure) return true
+             else return false
+            // state.figures = clickedEnemyFigure ? state.figures.filter(f => f.Id !== clickedEnemyFigure.Id) : state.figures;
+        }
+        else return false;
+        return false
+        // const maxMove = (activeFigure.Player === Player.White && activeFigure.Row === RowLine.Two) || (activeFigure.Player === Player.Black && activeFigure.Row === RowLine.Seven) ? 2 : 1
+        
+        // else if (clickedRow === lastLine) { state.activeFigure.Type = FigureType.Queen; break}
+        // else if (state.figures.some(f => f.Column === clickedCol && f.Row === clickedRow + direction )) return state
+        // else if (activeFigure.Column === clickedCol && activeFigure.Row === clickedRow + maxMove*direction) break
+       }
+
+       function kingMoves(activeFigure : Figure){
+
+       }
+
+       function queenMoves(activeFigure : Figure){
+
+       }
+
+       function bishopMoves(activeFigure : Figure){
+
+       }
+
+       function knightMoves(activeFigure : Figure){
+
+       }
+
+       function rookMoves(activeFigure : Figure){
+         
+       }
+
+      function isMoveAllowed(){
+        const activeFigure = state.activeFigure;
+        if (!activeFigure) return false;
+
+        let allowed = false
+
+        switch (activeFigure.Type){
+          case FigureType.Pawn: allowed = pawnMoves(activeFigure); break
+          case FigureType.Rook: rookMoves(activeFigure); break
+          case FigureType.Queen: queenMoves(activeFigure); break
+          case FigureType.King: kingMoves(activeFigure); break
+          case FigureType.Bishop: bishopMoves(activeFigure); break
+          case FigureType.Knight: knightMoves(activeFigure); break
+        }
+        return allowed
+      }
+
+      if (!anyActiveFigure()){
+        if (isPlayersFigure()) {
+          playerClickedHisFigureFirstTime()
+          return state
         }
       }
       else {
-        if (state.activeFigure.Id === clickedTileFigure?.Id){  
-          state.activeFigure = undefined
-          return state;
+        if (isClickedFigureAndActiveFigureTheSame()){  
+          playerClickedThisFigureSecondTime()
+          return state
         }
-        if (!clickedTileFigure) {
-          const fi = state.figures.findIndex(f => state.activeFigure?.Column === f.Column && state.activeFigure?.Row === f.Row )
-          state.figures[fi].Column = col
-          state.figures[fi].Row = row
-          state.activeFigure = undefined
-          state.currentPlayerTurn = changeTurn(state.currentPlayerTurn);
-          return state;
-        }
-        else {
-          if (clickedTileFigure.Player !== state.currentPlayerTurn){
-
-            const enemyFigure = state.figures.find(f => f.Id === clickedTileFigure.Id )
-            state.figures = state.figures.filter(f => f.Id !== enemyFigure?.Id);
-
-            const fi = state.figures.findIndex(f => state.activeFigure?.Column === f.Column && state.activeFigure?.Row === f.Row )
-            state.figures[fi].Column = col
-            state.figures[fi].Row = row
-            state.activeFigure = undefined
-            state.currentPlayerTurn = changeTurn(state.currentPlayerTurn);
-            return state;
-          }
-        }
+        if (!isMoveAllowed()) return state
+        else{
+          isEnemyFigureClicked() ? playerRemovesEnemysFigure() : moveFigure()
+        } 
       }
     },
   },
--- src/board/Interface.tsx
+++ src/board/Interface.tsx
@@ -1,7 +1,7 @@
 export enum Player { White, Black }
 export enum FigureType { Pawn, Rook, Knight, Bishop, Queen, King }
-export enum RowLine { One = 1, Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8 }
-export enum ColumnLine { A = "A", B = "B", C = "C", D = "D", E = "E", F = "F", G = "G", H = "H" }
+export enum RowLine { One = 1, Two, Three, Four, Five, Six, Seven, Eight }
+export enum ColumnLine { A = 1, B, C, D, E, F, G, H }
 export const Rows: Array<RowLine> = [RowLine.Eight, RowLine.Seven, RowLine.Six, RowLine.Five, RowLine.Four, RowLine.Three, RowLine.Two, RowLine.One];
 export const Columns: Array<ColumnLine> = [ColumnLine.A, ColumnLine.B, ColumnLine.C, ColumnLine.D, ColumnLine.E, ColumnLine.F, ColumnLine.G, ColumnLine.H];
 
@@ -25,12 +25,12 @@ export const InitFigures: Array<Figure> = [
     { Id: 8, Type: FigureType.Pawn, Player: Player.White, Row: RowLine.Two, Column: ColumnLine.H, ImgPath: "wpawn.png" },
 
     { Id: 9, Type: FigureType.Rook, Player: Player.White, Row: RowLine.One, Column: ColumnLine.A, ImgPath: "wrook.png" },
-    { Id: 10, Type: FigureType.Knight, Player: Player.White, Row: RowLine.One, Column: ColumnLine.B, ImgPath: "wbishop.png" },
-    { Id: 11, Type: FigureType.Bishop, Player: Player.White, Row: RowLine.One, Column: ColumnLine.C, ImgPath: "wknight.png" },
+    { Id: 10, Type: FigureType.Knight, Player: Player.White, Row: RowLine.One, Column: ColumnLine.B, ImgPath: "wknight.png" },
+    { Id: 11, Type: FigureType.Bishop, Player: Player.White, Row: RowLine.One, Column: ColumnLine.C, ImgPath: "wbishop.png" },
     { Id: 12, Type: FigureType.Queen, Player: Player.White, Row: RowLine.One, Column: ColumnLine.D, ImgPath: "wqueen.png" },
     { Id: 13, Type: FigureType.King, Player: Player.White, Row: RowLine.One, Column: ColumnLine.E, ImgPath: "wking.png" },
-    { Id: 14, Type: FigureType.Bishop, Player: Player.White, Row: RowLine.One, Column: ColumnLine.F, ImgPath: "wknight.png" },
-    { Id: 15, Type: FigureType.Knight, Player: Player.White, Row: RowLine.One, Column: ColumnLine.G, ImgPath: "wbishop.png" },
+    { Id: 14, Type: FigureType.Bishop, Player: Player.White, Row: RowLine.One, Column: ColumnLine.F, ImgPath: "wbishop.png" },
+    { Id: 15, Type: FigureType.Knight, Player: Player.White, Row: RowLine.One, Column: ColumnLine.G, ImgPath: "wknight.png" },
     { Id: 16, Type: FigureType.Rook, Player: Player.White, Row: RowLine.One, Column: ColumnLine.H, ImgPath: "wrook.png" },
 
     { Id: 17, Type: FigureType.Pawn, Player: Player.Black, Row: RowLine.Seven, Column: ColumnLine.A, ImgPath: "bpawn.png" },
@@ -43,11 +43,11 @@ export const InitFigures: Array<Figure> = [
     { Id: 24, Type: FigureType.Pawn, Player: Player.Black, Row: RowLine.Seven, Column: ColumnLine.H, ImgPath: "bpawn.png" },
 
     { Id: 25, Type: FigureType.Rook, Player: Player.Black, Row: RowLine.Eight, Column: ColumnLine.A, ImgPath: "brook.png" },
-    { Id: 26, Type: FigureType.Knight, Player: Player.Black, Row: RowLine.Eight, Column: ColumnLine.B, ImgPath: "bbishop.png" },
-    { Id: 27, Type: FigureType.Bishop, Player: Player.Black, Row: RowLine.Eight, Column: ColumnLine.C, ImgPath: "bknight.png" },
+    { Id: 26, Type: FigureType.Knight, Player: Player.Black, Row: RowLine.Eight, Column: ColumnLine.B, ImgPath: "bknight.png" },
+    { Id: 27, Type: FigureType.Bishop, Player: Player.Black, Row: RowLine.Eight, Column: ColumnLine.C, ImgPath: "bbishop.png" },
     { Id: 28, Type: FigureType.Queen, Player: Player.Black, Row: RowLine.Eight, Column: ColumnLine.D, ImgPath: "bqueen.png" },
     { Id: 29, Type: FigureType.King, Player: Player.Black, Row: RowLine.Eight, Column: ColumnLine.E, ImgPath: "bking.png" },
-    { Id: 30, Type: FigureType.Bishop, Player: Player.Black, Row: RowLine.Eight, Column: ColumnLine.F, ImgPath: "bknight.png" },
-    { Id: 31, Type: FigureType.Knight, Player: Player.Black, Row: RowLine.Eight, Column: ColumnLine.G, ImgPath: "bbishop.png" },
+    { Id: 30, Type: FigureType.Bishop, Player: Player.Black, Row: RowLine.Eight, Column: ColumnLine.F, ImgPath: "bbishop.png" },
+    { Id: 31, Type: FigureType.Knight, Player: Player.Black, Row: RowLine.Eight, Column: ColumnLine.G, ImgPath: "bknight.png" },
     { Id: 32, Type: FigureType.Rook, Player: Player.Black, Row: RowLine.Eight, Column: ColumnLine.H, ImgPath: "brook.png" }
 ]
